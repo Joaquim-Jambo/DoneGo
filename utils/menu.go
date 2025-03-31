b458a073cd3e1ae1a8b8b8b06ad6772e7d353592
Joaquim-Jambo <joaquimjambo12@gmail.com>
2025-03-31 15:35:14 +0100

    add funcao menu

---
+++ utils/menu.go
@@ -0,0 +1,15 @@
+package utils
+
+import "fmt"
+
+func menu() {
+	fmt.Println("===================================")
+	fmt.Println()
+	fmt.Println("1)✅ Listar todos os Todos")
+	fmt.Println("2)📂 Listar por Categoria")
+	fmt.Println("3)📝 Adicionar Novo Todo")
+	fmt.Println("4)✏️ Editar Todo")
+	fmt.Println("5)❌ Deletar Todo")
+	fmt.Println()
+	fmt.Println("===================================")
+}
