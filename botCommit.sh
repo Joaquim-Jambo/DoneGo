1e151bd48dfe0f2cb9b07f61c4767643b33cb718
Joaquim-Jambo <joaquimjambo12@gmail.com>
2025-04-01 12:22:57 +0100

    DoneGoV1

--- botCommit.sh
+++ botCommit.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+# Atualiza o repositório local com as alterações do repositório remoto
+git pull origin main
+
+# Adiciona todas as alterações
 git add .
+
+# Faz o commit com uma mensagem vazia
 git commit -a --allow-empty-message -m ""
-git push
+
+# Envia as alterações para o repositório remoto
+git push origin main
